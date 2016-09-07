2eb55563d82b61ddb701864a75f5964d70c3dd11
Kyle Braver <kyle.braver@gmail.com>
2016-09-07 00:31:24 -0700

    Add #my_select, #my_all?, #my_any?, #my_none?, #my_count, #my_map to Enumerable.

--- enumerable/enumerable.rb
+++ enumerable/enumerable.rb
@@ -1,7 +1,7 @@
 module Enumerable 
 	def my_each
 		for item in self
-			yield item
+			yield(item)
 		end
 		return self
 	end
@@ -9,8 +9,58 @@ module Enumerable
 	def my_each_with_index
 		index = 0
 		for item in self
-			yield item, index
+			yield(item, index)
 			index += 1
 		end 
 	end
+
+	def my_select
+		selection = []
+		self.my_each do |value|
+			selection << value if yield(value) 
+		end
+		return selection
+	end
+
+	def my_all?
+		self.my_each do |value|
+			return false if !yield(value)
+		end
+		return true
+	end
+
+	def my_any?
+		self.my_each do |value|
+			return true if yield(value)
+		end
+		return false
+	end
+
+	def my_none?
+		self.my_each do |value|
+			return false if yield(value)
+		end
+		return true
+	end
+
+	def my_count
+		count = 0
+		self.my_each do |value|
+			if(block_given?)
+				count += 1 if yield(value)
+			else
+				count += 1
+			end
+		end
+		return count 
+	end
+
+	def my_map
+		mapped = []
+		self.my_each do |value|
+			mapped << yield(value)
+		end
+		return mapped
+	end
+
 end
