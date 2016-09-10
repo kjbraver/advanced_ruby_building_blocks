9d8aaeb3679294d222379b250bcd2a05e6e93e73
Kyle Braver <kyle.braver@gmail.com>
2016-09-09 23:23:11 -0700

    Fix tab/spacing display issue.

--- bubble_sort/sorting.rb
+++ bubble_sort/sorting.rb
@@ -1,41 +1,41 @@
 #can only accept numerical values
 def bubble_sort(unsorted)
-	bubble_sort_by(unsorted) {|first, second| second - first}
+  bubble_sort_by(unsorted) {|first, second| second - first}
 end
 
 def bubble_sort_by(unsorted)
-	#validate unsorted is an array
-	error = "Wrong Argument Error: This function accepts an array of unsorted objects as input."
-	return error unless unsorted.is_a?(Array)
+  #validate unsorted is an array
+  error = "Wrong Argument Error: This function accepts an array of unsorted objects as input."
+  return error unless unsorted.is_a?(Array)
 
-	copy = unsorted
-	sorted = false
-	#shorten scope of sort with each pass of the loop (i.e. ignore last spot, then next last...)
-	sorted_spots = 0
-	
-	until(sorted)
-		sorted = true
-		#calc scope of scan. it is the size of the array, adjusted to index at 0,
-		#adjusted for the spots at the end of the array that by definition hold sorted values
-		scope_of_scan = copy.size - 1 - sorted_spots
-		
-		#compare each value to next value until a full pass is completed with no out of place elements
-		scope_of_scan.times do |index|
-			#will trigger if the yeild block results in a positive number
-			if(yield(copy[index], copy[index + 1])) > 0
-				sorted = false
-				copy[index], copy[index+1] = copy[index+1], copy[index]
-			end
-		end
+  copy = unsorted
+  sorted = false
+  #shorten scope of sort with each pass of the loop (i.e. ignore last spot, then next last...)
+  sorted_spots = 0
+  
+  until(sorted)
+    sorted = true
+    #calc scope of scan. it is the size of the array, adjusted to index at 0,
+    #adjusted for the spots at the end of the array that by definition hold sorted values
+    scope_of_scan = copy.size - 1 - sorted_spots
+    
+    #compare each value to next value until a full pass is completed with no out of place elements
+    scope_of_scan.times do |index|
+      #will trigger if the yeild block results in a positive number
+      if(yield(copy[index], copy[index + 1])) > 0
+        sorted = false
+        copy[index], copy[index+1] = copy[index+1], copy[index]
+      end
+    end
 
-		#decrease scope of scan by one 
-		sorted_spots += 1
-	end
+    #decrease scope of scan by one 
+    sorted_spots += 1
+  end
 
-	return copy
+  return copy
 end
 
 #testing
 p (bubble_sort_by(["hi","hello","hey"]) do |left,right|
-	left.length - right.length 
+  left.length - right.length 
 end)
--- enumerable/enumerable.rb
+++ enumerable/enumerable.rb
@@ -1,91 +1,91 @@
 module Enumerable 
-	def my_each
-		for item in self
-			yield(item)
-		end
-		return self
-	end
+  def my_each
+    for item in self
+      yield(item)
+    end
+    return self
+  end
 
-	def my_each_with_index
-		index = 0
-		for item in self
-			yield(item, index)
-			index += 1
-		end 
-	end
+  def my_each_with_index
+    index = 0
+    for item in self
+      yield(item, index)
+      index += 1
+    end 
+  end
 
-	def my_select
-		selection = []
-		self.my_each do |value|
-			selection << value if yield(value) 
-		end
-		return selection
-	end
+  def my_select
+    selection = []
+    self.my_each do |value|
+      selection << value if yield(value) 
+    end
+    return selection
+  end
 
-	def my_all?
-		self.my_each do |value|
-			return false if !yield(value)
-		end
-		return true
-	end
+  def my_all?
+    self.my_each do |value|
+      return false if !yield(value)
+    end
+    return true
+  end
 
-	def my_any?
-		self.my_each do |value|
-			return true if yield(value)
-		end
-		return false
-	end
+  def my_any?
+    self.my_each do |value|
+      return true if yield(value)
+    end
+    return false
+  end
 
-	def my_none?
-		self.my_each do |value|
-			return false if yield(value)
-		end
-		return true
-	end
+  def my_none?
+    self.my_each do |value|
+      return false if yield(value)
+    end
+    return true
+  end
 
-	def my_count
-		count = 0
-		self.my_each do |value|
-			if(block_given?)
-				count += 1 if yield(value)
-			else
-				count += 1
-			end
-		end
-		return count 
-	end
+  def my_count
+    count = 0
+    self.my_each do |value|
+      if(block_given?)
+        count += 1 if yield(value)
+      else
+        count += 1
+      end
+    end
+    return count 
+  end
 
-	def my_map(instructions=nil)
-		mapped = []
-		self.my_each do |value|
-			mapped << yield(value) if instructions.nil?
-			mapped << instructions.call(value) if !instructions.nil?
-		end
-		return mapped
-	end
+  def my_map(instructions=nil)
+    mapped = []
+    self.my_each do |value|
+      mapped << yield(value) if instructions.nil?
+      mapped << instructions.call(value) if !instructions.nil?
+    end
+    return mapped
+  end
 
-	def my_inject(initial=nil, instructions=nil) 
-		data = self.to_a
-		total = 0
-			if(initial.nil?)
-				total = data[0]
-				if(instructions.nil?) 
-					data.drop(1).my_each {|value| total = yield(total, value)} 
-				else
-					data.drop(1).my_each {|value| total = instructions.call(total, value)}
-				end
-			else
-				total = initial
-				if(instructions.nil?) 
-					data.my_each {|value| total = yield(total, value)} 
-				else
-					data.my_each {|value| total = instructions.call(total, value)}
-				end
-			end
-		return total
-	end
+  def my_inject(initial=nil, instructions=nil) 
+    data = self.to_a
+    total = 0
+      if(initial.nil?)
+        total = data[0]
+        if(instructions.nil?) 
+          data.drop(1).my_each {|value| total = yield(total, value)} 
+        else
+          data.drop(1).my_each {|value| total = instructions.call(total, value)}
+        end
+      else
+        total = initial
+        if(instructions.nil?) 
+          data.my_each {|value| total = yield(total, value)} 
+        else
+          data.my_each {|value| total = instructions.call(total, value)}
+        end
+      end
+    return total
+  end
 end
 
 def multiply_els(numbers)
-	numbers.my_inject(1) {|product, num| product * num}
+  numbers.my_inject(1) {|product, num| product * num}
 end
